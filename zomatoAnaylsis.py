f96b3557942c7efef2f615b9bf5844386c5870d6
Kartik <chandorakartik@gmail.com>
2026-08-20 22:12:47 +0530

    Data Transformation / Preprocessing

--- zomatoAnaylsis.py
+++ zomatoAnaylsis.py
@@ -32,3 +32,26 @@ print(df1.drop_duplicates(inplace=True))
 
 print("Duplicated:",df1.duplicated().sum())
 
+# Data Transformation / Preprocessing
+
+print("Data type of rate:",df1['rate'].dtype)
+
+parts = df1['rate'].str.split('/')
+df1['rate'] = parts.str[0].astype(float)
+
+print("Updated Data type of rate:",df1['rate'].dtype)
+
+print(df1.head())
+df1.info()
+
+def conversion(value):
+    if(value == 'Yes'):
+        value = 1
+    else:
+        value = 0
+    return value
+
+df1['online_order'] = df1['online_order'].apply(conversion)
+
+print(df1.head())
+df1.info()
