eef8cb3cd0b8919e55218718dd76d06e0abfb1e2
Kartik <chandorakartik@gmail.com>
2026-08-20 20:49:16 +0530

    Cleaning the dataset

--- zomatoAnaylsis.py
+++ zomatoAnaylsis.py
@@ -3,11 +3,32 @@ import pandas as pd
 
 #Load the data
 df = pd.read_csv("Zomato-data-.csv")
+ 
+pd.set_option('display.max_columns', None)
+
+pd.set_option('display.width', None)
+
 print(df.head())
 
 print(df.shape)
 
 print(df.columns)
 
+#Cleaning the dataset.
+print(df.isnull().head().sum())
+
 print(df.info())
+
+print(df.describe())
+
+df1 = df.drop(columns=['book_table','listed_in(type)'])
+
+print(df1.head())
+print("\n")
+
+print("Duplicated:",df1.duplicated().sum())
+
+print(df1.drop_duplicates(inplace=True))
+
+print("Duplicated:",df1.duplicated().sum())
 
